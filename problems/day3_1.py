9d0c34fa48eed9bdc481c67fcbf9074a1c45039c
Paul Draper <paulddraper@gmail.com>
2020-12-09 23:25:37 -0700

    Day 3

---
+++ problems/day3_1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+grid = [['#' == c for c in line.strip()] for line in sys.stdin if line]
+
+count = 0
+for i in range(1, len(grid)):
+    row = grid[i]
+    if row[i * 3 % len(row)]:
+        count += 1
+print(count)
---
+++ problems/day3_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+
+grid = [['#' == c for c in line.strip()] for line in sys.stdin if line]
+
+def count(right, down):
+    count = 0
+    for i in range(1, (len(grid) - 1) // down + 1):
+        row = grid[i * down]
+        if row[i * right % len(row)]:
+            count += 1
+    return count
+
+result = 1
+result *= count(1, 1)
+result *= count(3, 1)
+result *= count(5, 1)
+result *= count(7, 1)
+result *= count(1, 2)
+
+print(result)
